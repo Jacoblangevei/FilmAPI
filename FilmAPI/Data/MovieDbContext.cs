90912bcf05d4985abaf3369e8d32b7787db7ea02
kasbrudal <peephyt@gmail.com>
2023-09-26 12:56:22 +0200

    Added some info

--- FilmAPI/Data/MovieDbContext.cs
+++ FilmAPI/Data/MovieDbContext.cs
@@ -1,5 +1,5 @@
 ﻿
-using FilmAPI.Data.Entities;
+using FilmAPI.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -8,31 +8,77 @@ namespace FilmAPI.Data
 {
     public class MovieDbContext : DbContext
     {
-
         public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
         {
         }
-
         public DbSet<Character> Characters { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Configure relationships and other model-specific settings here
-            modelBuilder.Entity<MovieCharacter>()
-                .HasKey(mc => new { mc.MovieId, mc.CharacterId });
 
-            modelBuilder.Entity<MovieCharacter>()
-                .HasOne(mc => mc.Movie)
-                .WithMany(m => m.MovieCharacters)
-                .HasForeignKey(mc => mc.MovieId);
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 1,
+                    Title = "Fellowship of the ring",
+                    Genre = "Epic fantasy",
+                    ReleaseYear = 2001,
+                    Director = "Petter Jackson",
+                    PictureUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/The_Two_Towers_cover.gif/220px-The_Two_Towers_cover.gif",
+                    TrailerUrl = "https://www.youtube.com/watch?v=V75dMMIW2B4&ab_channel=Movieclips",
+                    FranchiseId = 1 
+                },
+                new Movie
+                {
+                    Id = 2,
+                    Title = "Two towers",
+                    Genre = "Epic fantasy",
+                    ReleaseYear = 2003,
+                    Director = "Petter Jackson",
+                    PictureUrl = "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+                    TrailerUrl = "https://www.youtube.com/watch?v=V75dMMIW2B4&ab_channel=Movieclips",
+                    FranchiseId = 1 
+                }
+            );
+            modelBuilder.Entity<Franchise>().HasData(
+                new Franchise
+                {
+                    Id = 1,
+                    Name = "Lord of the Rings",
+                    Description = "The epic fantasy franchise based on J.R.R. Tolkien's novels."
+                },
+                new Franchise
+                {
+                    Id = 2,
+                    Name = "Star Wars",
+                    Description = "The iconic space opera franchise created by George Lucas."
+                }
+             );
+            modelBuilder.Entity<Character>().HasData(
+                new Character
+                {
+                    Id = 1,
+                    FullName = "Frodo Baggins",
+                    Alias = "Ring-Bearer",
+                    Gender = "Male",
+                    PictureUrl = "https://example.com/frodo.jpg"
+                },
+                new Character
+                {
+                    Id = 2,
+                    FullName = "Aragorn",
+                    Alias = "Strider",
+                    Gender = "Male",
+                    PictureUrl = "https://example.com/aragorn.jpg"
+                }
+            );
 
-            modelBuilder.Entity<MovieCharacter>()
-                .HasOne(mc => mc.Character)
-                .WithMany(c => c.MovieCharacters)
-                .HasForeignKey(mc => mc.CharacterId);
+
+
         }
+
     }
 }
 
