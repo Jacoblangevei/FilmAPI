efa7bde6c42f0452d447c41b38b7e23cc3fa37fc
kasbrudal <peephyt@gmail.com>
2023-09-26 11:44:22 +0200

    Models and dbcontext made

---
+++ FilmAPI/Data/Entities/Characters.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmAPI.Data.Entities
+{
+    public class Characters
+    {
+        public class Character
+        {
+            [Key]
+            public int Id { get; set; }
+            public string FullName { get; set; }
+            public string Alias { get; set; }
+            public string Gender { get; set; }
+            public string PictureUrl { get; set; }
+
+            // Navigation property
+            public ICollection<MovieCharacter> MovieCharacters { get; set; }
+        }
+    }
+}
---
+++ FilmAPI/Data/MovieDbContext.cs
@@ -0,0 +1,41 @@
+﻿
+using FilmAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FilmAPI.Data
+
+{
+    public class MovieDbContext : DbContext
+    {
+
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Characters> Characters { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<Franchise> Franchises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure relationships and other model-specific settings here
+            modelBuilder.Entity<MovieCharacter>()
+                .HasKey(mc => new { mc.MovieId, mc.CharacterId });
+
+            modelBuilder.Entity<MovieCharacter>()
+                .HasOne(mc => mc.Movie)
+                .WithMany(m => m.MovieCharacters)
+                .HasForeignKey(mc => mc.MovieId);
+
+            modelBuilder.Entity<MovieCharacter>()
+                .HasOne(mc => mc.Character)
+                .WithMany(c => c.MovieCharacters)
+                .HasForeignKey(mc => mc.CharacterId);
+        }
+    }
+}
+
+
+
+
